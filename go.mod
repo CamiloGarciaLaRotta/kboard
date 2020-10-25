fa4956b918104b9b8b108203432362dd13debdae
Camilo Garcia La Rotta <camilogarcialarotta@github.com>
2020-10-25 14:23:59 -0400

    feat: initial single round game

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/CamiloGarciaLaRotta/kboard
+
+go 1.15
+
+require (
+	github.com/charmbracelet/bubbles v0.7.1
+	github.com/charmbracelet/bubbletea v0.12.1
+	github.com/onsi/ginkgo v1.14.2 // indirect
+	github.com/onsi/gomega v1.10.3 // indirect
+	github.com/tjarratt/babble v0.0.0-20191209142150-eecdf8c2339d
+)
---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	input "github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+
+	"github.com/tjarratt/babble"
+)
+
+const usage = `kboard [number]
+
+number: the number of words to generate. Must be a non-zero positive integer.
+
+Example: kboard 2`
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println(usage)
+		os.Exit(1)
+	}
+
+	if os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Println(usage)
+		os.Exit(0)
+	}
+
+	numOfWords, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+
+	if numOfWords < 1 {
+		fmt.Println(usage)
+		os.Exit(1)
+	}
+
+	p := tea.NewProgram(initialModel(numOfWords))
+
+	if err := p.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type model struct {
+	babbler     babble.Babbler
+	textInput   input.Model
+	currentWord string
+}
+
+func initialModel(numOfWords int) model {
+	inputModel := input.NewModel()
+	babbler := babble.NewBabbler()
+	babbler.Separator = " "
+	babbler.Count = numOfWords
+	inputModel.Placeholder = "Type the word above and press Enter 👆🏽"
+	inputModel.Focus()
+
+	return model{
+		babbler:     babbler,
+		currentWord: babbler.Babble(),
+		textInput:   inputModel,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return input.Blink(m.textInput)
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEsc, tea.KeyCtrlC:
+			return m, tea.Quit
+		case tea.KeyEnter:
+			if m.textInput.Value() == m.currentWord {
+				result := fmt.Sprintf("%s\n🎉 correct!", m.textInput.Value())
+				m.textInput.SetValue(result)
+				m.textInput.Blur()
+			} else {
+				result := fmt.Sprintf("%s\n😭 nope", m.textInput.Value())
+				m.textInput.SetValue(result)
+				m.textInput.Blur()
+			}
+			return m, tea.Quit
+		}
+
+	}
+
+	m.textInput, cmd = input.Update(msg, m.textInput)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"%s\n\n\n%s\n\n%s",
+		m.currentWord,
+		input.View(m.textInput),
+		"(esc or ctrl-c to quit)",
+	) + "\n"
+}
